4bf47543c23e73deecb918c6cf140437c8b96491
ryan-bibb <ryanbibb34@gmail.com>
2026-09-01 11:26:55 -0500

    fix: invite page title

--- app/(auth)/docs/invites/page.tsx
+++ app/(auth)/docs/invites/page.tsx
@@ -18,20 +18,25 @@ export default async function InvitesPage() {
   })
 
   return (
-    <div className="flex flex-row flex-wrap gap-5 p-5 w-full border-y">
-      {documents.length === 0 && (
-        <div>There are currently no documents shared with you</div>
-      )}
-      {documents.map((document) => (
-        <div key={document.documentId}>
-          <DocCard
-            title={document.title}
-            date={document.updatedAt}
-            docId={document.documentId}
-            userId={document.creatorId}
-          />
-        </div>
-      ))}
+    <div className="flex flex-col p-5">
+      <div className="p-3">
+        <h1>Invites</h1>
+      </div>
+      <div className="flex flex-row flex-wrap gap-5 p-5 w-full border-y">
+        {documents.length === 0 && (
+          <div>There are currently no documents shared with you</div>
+        )}
+        {documents.map((document) => (
+          <div key={document.documentId}>
+            <DocCard
+              title={document.title}
+              date={document.updatedAt}
+              docId={document.documentId}
+              userId={document.creatorId}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
